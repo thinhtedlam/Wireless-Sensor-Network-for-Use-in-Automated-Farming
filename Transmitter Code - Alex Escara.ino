abacd0a80e1bce965bd82d52c6947c175f4bc49f
Thinh Lam <the.avatar1992@gmail.com>
2017-02-22 22:22:37 -1000

    Add files via upload

---
+++ Transmitter Code - Alex Escara.ino
@@ -0,0 +1,184 @@
+// Feather9x_TX
+// -*- mode: C++ -*-
+// Example sketch showing how to create a simple messaging client (transmitter)
+// with the RH_RF95 class. RH_RF95 class does not provide for addressing or
+// reliability, so you should only use RH_RF95 if you do not need the higher
+// level messaging abilities.
+// It is designed to work with the other example Feather9x_RX
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include "Adafruit_TSL2591.h"
+#include <Adafruit_BME280.h>
+
+#include <SPI.h>
+#include <RH_RF95.h>
+
+/* for feather m0 */
+#define RFM95_CS 8
+#define RFM95_RST 4
+#define RFM95_INT 3
+
+// Change to 434.0 or other frequency, must match RX's freq!
+#define RF95_FREQ 915.0
+
+// Singleton instance of the radio driver
+RH_RF95 rf95(RFM95_CS, RFM95_INT);
+
+Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_BME280 bme; // I2C
+
+void displaySensorDetails(void) //For TSL sensor
+{
+  sensor_t sensor;
+  tsl.getSensor(&sensor);
+  Serial.println("------------------------------------");
+  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
+  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
+  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
+  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" lux");
+  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" lux");
+  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" lux");  
+  Serial.println("------------------------------------");
+  Serial.println("");
+  delay(500);
+}
+
+void configureSensor(void) //For TSL sensor
+{
+  // You can change the gain on the fly, to adapt to brighter/dimmer light situations
+  //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
+  tsl.setGain(TSL2591_GAIN_MED);      // 25x gain
+  // tsl.setGain(TSL2591_GAIN_HIGH);   // 428x gain
+  
+  // Changing the integration time gives you a longer time over which to sense light
+  // longer timelines are slower, but are good in very low light situtations!
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
+  // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
+
+  /* Display the gain and integration time for reference sake */  
+  Serial.println("------------------------------------");
+  Serial.print  ("Gain:         ");
+  tsl2591Gain_t gain = tsl.getGain();
+  switch(gain)
+  {
+    case TSL2591_GAIN_LOW:
+      Serial.println("1x (Low)");
+      break;
+    case TSL2591_GAIN_MED:
+      Serial.println("25x (Medium)");
+      break;
+    case TSL2591_GAIN_HIGH:
+      Serial.println("428x (High)");
+      break;
+    case TSL2591_GAIN_MAX:
+      Serial.println("9876x (Max)");
+      break;
+  }
+  Serial.print  ("Timing:       ");
+  Serial.print((tsl.getTiming() + 1) * 100, DEC); 
+  Serial.println(" ms");
+  Serial.println("------------------------------------");
+  Serial.println("");
+}
+
+void setup() 
+{
+  pinMode(RFM95_RST, OUTPUT);
+  digitalWrite(RFM95_RST, HIGH);
+
+  /*while (!Serial);
+  
+  delay(100);*/
+  Serial.begin(9600);
+
+  Serial.println("Feather LoRa TX Test!");
+
+  // manual reset
+  digitalWrite(RFM95_RST, LOW);
+  delay(10);
+  digitalWrite(RFM95_RST, HIGH);
+  delay(10);
+
+  while (!rf95.init()) {
+    Serial.println("LoRa radio init failed");
+    while (1);
+  }
+  Serial.println("LoRa radio init OK!");
+
+  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
+  if (!rf95.setFrequency(RF95_FREQ)) {
+    Serial.println("setFrequency failed");
+    while (1);
+  }
+  Serial.print("Set Freq to: "); Serial.println(RF95_FREQ);
+  
+  // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on
+
+  // The default transmitter power is 13dBm, using PA_BOOST.
+  // If you are using RFM95/96/97/98 modules which uses the PA_BOOST transmitter pin, then 
+  // you can set transmitter powers from 5 to 23 dBm:
+  rf95.setTxPower(23, false);
+
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+
+  /* Configure the sensor */
+  configureSensor();
+
+  Serial.println(F("BME280 test"));
+
+  if (!bme.begin()) {
+    Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    while (1);
+  }
+}
+
+void loop()
+{
+  Serial.println("Sending to receiver...");
+  Serial.println();
+  
+  uint16_t light = tsl.getLuminosity(TSL2591_VISIBLE);
+  char lightdata[20]="Lux: ";
+  itoa(light, lightdata+5, 10);
+  Serial.print("Sending "); Serial.println(lightdata);
+
+  uint16_t temp = bme.readTemperature();
+  char tempdata[20] = "*C: ";
+  itoa(temp, tempdata+4, 10);
+  Serial.print("Sending "); Serial.println(tempdata);
+
+  uint16_t humidity = bme.readHumidity();
+  char humidata[20]="Humidity %: ";
+  itoa(humidity, humidata+12, 10);
+  Serial.print("Sending "); Serial.println(humidata);
+
+  uint16_t pressure = (bme.readPressure())/100;
+  char hpadata[20] = "hPa: ";
+  itoa(pressure, hpadata+5, 10);
+  Serial.print("Sending "); Serial.println(hpadata);
+  
+  Serial.println();
+  rf95.send((uint8_t *)lightdata, 20); //Actually sending data wirelessly
+  rf95.send((uint8_t *)tempdata, 20);
+  rf95.send((uint8_t *)humidata, 20);
+  rf95.send((uint8_t *)hpadata, 20);
+  Serial.println("Waiting for packet to complete..."); delay(10);
+  rf95.waitPacketSent();
+  delay(5000);
+  Serial.println();
+  Serial.println();
+}
